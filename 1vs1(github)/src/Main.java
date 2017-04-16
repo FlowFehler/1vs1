78604d248abde58ede53deb77d7ce6ba4e36d40f
alana <alana@DESKTOP-SREOEN1>
2017-04-16 17:18:58 +0200

    update 0.1

--- 1vs1(github)/src/Main.java
+++ 1vs1(github)/src/Main.java
@@ -19,20 +19,17 @@ import net.minecraft.server.v1_11_R1.PacketPlayInClientCommand.EnumClientCommand
 
 public class Main extends JavaPlugin implements Listener {
 
-	public static GameState gs = GameState.LOBBY;
+	Location l = new Location(Bukkit.getWorld("world"), 8.5, 4, 8.5);
+	private static GameState gs = GameState.LOBBY;
 	int ct;
 	int c = 30;
 	
 	public void onEnable() {
-		cd();
 		getServer().getPluginManager().registerEvents(this, this);
+		Countdown();
 	}
 	
-	/*
-	 * Methods
-	 */
-	
-	void tp (Location middle, int radius) {
+	private void TeleportPlayers (Location middle, int radius) {
 		float degree = 360 / Bukkit.getOnlinePlayers().size();
 		int i = 0;
 		for (Player p : Bukkit.getOnlinePlayers()) {
@@ -45,38 +42,34 @@ public class Main extends JavaPlugin implements Listener {
 		}
 	}
 	
-	void respawn(Player p) {
+	private void FasterRespawn (Player p) {
 		Bukkit.getScheduler().runTaskLater(this, new Runnable() {
 			public void run() {
 				((CraftPlayer)p).getHandle().playerConnection.a(new PacketPlayInClientCommand(EnumClientCommand.PERFORM_RESPAWN));
-				Location l = new Location(Bukkit.getWorld("world"), 8.5, 4, 8.5);
 				p.teleport(l);
 			}
 		}, 1);
 	}
 	
-	void reset() {
+	private void ResetGame() {
 		for (Player p : Bukkit.getOnlinePlayers()) {
 			p.getInventory().clear();
 			p.getInventory().setArmorContents(null);
-			Location l = new Location(Bukkit.getWorld("world"), 8.5, 4, 8.5);
 			p.teleport(l);
 		}
 		gs = GameState.LOBBY;
 		c = 30;
-		cd();
+		Countdown();
 	}
 	
-	void cd() {
+	private void Countdown() {
 		ct = getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
 			public void run() {
 				if (c == 0) {
 					if (Bukkit.getOnlinePlayers().size() == 2) {
 						getServer().getScheduler().cancelTask(ct);
-						gs = GameState.INGAME;
-						Location l = new Location(Bukkit.getWorld("world"), 8.5, 4, 8.5);
 						for (Player p : Bukkit.getOnlinePlayers()) {
-							tp(l, 15);
+							TeleportPlayers(l, 15);
 							p.getInventory().clear();
 							p.getInventory().setArmorContents(null);
 							p.getInventory().setHelmet(new ItemStack(Material.IRON_HELMET));
@@ -86,64 +79,48 @@ public class Main extends JavaPlugin implements Listener {
 							p.getInventory().addItem(new ItemStack(Material.STONE_SWORD));
 						}
 						Bukkit.broadcastMessage("§b[1vs1] §6Rundenstart jetzt!");
-						return;
+						gs = GameState.INGAME;
+					} else {
+						Bukkit.broadcastMessage("§b[1vs1] §cEs sind nicht genügend Spieler online, um das Spiel zu starten!");
+						c = 30;
 					}
-					Bukkit.broadcastMessage("§b[1vs1] §cEs sind nicht genügend Spieler online, um das Spiel zu starten!");
-					c = 30;
-				} if (c == 30 || c == 20 || c == 10 || c == 5 || c == 4 || c == 3 || c == 2) {
-					Bukkit.broadcastMessage("§b[1vs1] §6Rundenstart §7in §6" + c + " Sekunden§7!");
-				} else if (c == 1) {
-					Bukkit.broadcastMessage("§b[1vs1] §6Rundenstart §7in §61 Sekunde§7!");
+				} if (c == 30 || c == 20 || c == 10 || c == 5 || c == 4 || c == 3 || c == 2 || c == 1) {
+					Bukkit.broadcastMessage("§b[1vs1] §6Rundenstart §7in §6" + c + " Sekunde(n)§7!");
 				}
-				c--;
+				c--;	
 			}
 		}, 0L, 20L);
 	}
 	
-	
-	/*
-	 * Events
-	 */
-	
 	@EventHandler
-	public void onEntityDamage (EntityDamageEvent e) {
+	private void onEntityDamage (EntityDamageEvent e) {
 		if (gs != GameState.INGAME) {
 			e.setCancelled(true);
 		}
 	}
 	
 	@EventHandler
-	public void onPlayerDeath (PlayerDeathEvent e) {
+	private void onPlayerDeath (PlayerDeathEvent e) {
 		e.getDrops().clear();
+		e.setDeathMessage("§b[1vs1] §6" + e.getEntity().getKiller().getName() + " §7hat §agewonnen§7!");
 		Player p = e.getEntity().getPlayer();
-		respawn(p);
-		e.setDeathMessage("§b[1vs1] §6" + e.getEntity().getKiller().getDisplayName() + " §7hat §agewonnen§7!");
-		reset();
+		FasterRespawn(p);
+		ResetGame();
 	}
 	
 	@EventHandler
-	public void onPlayerJoin (PlayerJoinEvent e) {
+	private void onPlayerJoin (PlayerJoinEvent e) {
 		Player p = e.getPlayer();
-		Location l = new Location(Bukkit.getWorld("world"), 8.5, 4, 8.5);
 		p.teleport(l);
-		/*
-		 * Chat color
-		 */
-		if(!p.isOp()) {
-			p.setDisplayName("§7" + p.getName() + "§r");
-		} else {
-			p.setDisplayName("§c[Operator] " + p.getName() + "§r");
-		}
-		e.setJoinMessage("§b[1vs1] §a+" + p.getDisplayName());
 	}
 	
 	@EventHandler
-	public void onPlayerQuit (PlayerQuitEvent e) {
+	private void onPlayerQuit (PlayerQuitEvent e) {
 		Player p = e.getPlayer();
 		if (gs != GameState.LOBBY) {
 			Bukkit.broadcastMessage("§b[1vs1] §6Niemand hat gewonnen :c!");
-			reset();
+			ResetGame();
 		}
-		e.setQuitMessage("§b[1vs1] §c-" + p.getDisplayName());
+		e.setQuitMessage("§b[1vs1] §c-" + p.getName());
 	}
 }
